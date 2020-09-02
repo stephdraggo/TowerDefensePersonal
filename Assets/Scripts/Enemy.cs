02cc9489dda6280749095d262243af96654a03c1
stephdraggo <stephdraggo@users.noreply.github.com>
2020-09-02 12:44:14 +1000

    hmmmmmmmmmm

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tower
+{
+    [AddComponentMenu("Mechanics/Enemy")]
+    public class Enemy : MonoBehaviour
+    {
+        #region Variables
+        [Header("Reference Variables")]
+
+        [Header("General Stats")]
+        [SerializeField] private float health = 1;
+        [SerializeField] private float speed = 1;
+        [SerializeField] private float size = 1;
+
+        [Header("Attack Stats")]
+        [SerializeField] private float damage = 1;
+        //[SerializeField] private string[] resistances;
+
+        [Header("On Death")]
+        [SerializeField] private float xpGain = 1;
+        [SerializeField] private int moneyGain = 1;
+
+        #endregion
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,52 +4,108 @@ using UnityEngine;
 
 namespace Tower
 {
-    [AddComponentMenu("Mehanics/Tower")]
+    [AddComponentMenu("Mechanics/Tower")]
     public class Tower : MonoBehaviour
     {
         #region Variables
         [Header("Reference Variables")]
+        [Header("General Stats")]
+        [SerializeField] private string towerName = "";
+        [SerializeField, TextArea] private string description = "";
+        [SerializeField, Range(1, 10)] private int cost = 1;
 
-        public string towerName;
+        [Header("Experience Stats")]
+        [SerializeField, Range(2, 5)] private int maxLevel = 3;
+        [SerializeField, Min(1)] private float baseRequiredXp = 5;
+        [SerializeField, Min(1)] private float xpScaler = 1;
 
-        [Multiline]
-        public string description;
+        [Header("Attack Stats")]
+        [SerializeField, Min(0.1f)] private float damage = 1;
+        [SerializeField, Min(0.1f)] private float minRange = 1;
+        [SerializeField] private float maxRange = 5;
+        [SerializeField, Min(0.1f)] private float fireRate = 0.1f;
 
-        [Space]
+        private int level = 1;
+        private float xp;
+        private Enemy target;
 
-        [SerializeField]
-        [Range(-3, 3)]
-        [Tooltip("This variable controls how fast and in what direction the object moves up and down.")]
-        private float _speed;
+        #endregion
+        #region Properties
+        //get can access these variables but not change them
+        public string TowerName
+        {
+            get => towerName;
+        }
+        public string Description
+        {
+            get => description;
+        }
+        public int Cost
+        {
+            get => cost;
+        }
+        public string UiDisplayText
+        {
+            get
+            {
+                string display = string.Format("Name: {0} Cost: {1}\n", towerName, cost); //changes numbers to strings automatically
+                display += description + "\n";
+                display += string.Format("Min Range: {0}, Max Range: {1}, Damage: {2}", minRange, maxRange, damage);
 
-        [SerializeField]
-        [Min(0)]
-        private int _cost;
+                return display;
+            }
+        }
+        private float RequiredXp
+        {
+            get
+            {
+                if (level == 1)
+                {
+                    return baseRequiredXp;
+                }
+                return baseRequiredXp * level * xpScaler;
+            }
+        }
+        private float MinRange
+        {
+            get => minRange;
+        }
+        private float MaxRange
+        {
+            get => maxRange * (level * 0.5f + 0.5f); //multiply is faster than divide for computers
+        }
+        private float Damage
+        {
+            get => damage * (level * 0.5f + 0.5f);
+        }
+        #endregion
+        #region Unity Editor Functions
+#if UNITY_EDITOR
+        void OnValidate() //updates when inspector is changed
+        {
+            maxRange = Mathf.Clamp(maxRange, minRange + 1, float.MaxValue); //maxRange will always be at least 1 more than minRange
+        }
 
+        void OnDrawGizmosSelected() //draws only when object is selected
+        {
+            Gizmos.color = new Color(1, 0, 0, 0.5f); //transluscent red
+            Gizmos.DrawSphere(transform.position, minRange); //min range sphere
 
+            Gizmos.color = new Color(0, 0, 1, 0.25f); //transluscent blue
+            Gizmos.DrawSphere(transform.position, maxRange); //max range sphere
+        }
+#endif
         #endregion
         void Start()
         {
-            towerName = "Kevin";
-            _speed = Random.Range(0f, 3f);
-            _cost = 5;
+
         }
 
         void Update()
         {
-            transform.position += transform.up * Time.deltaTime * _speed * Input.GetAxis("Vertical");
+
         }
 
-        void OnDrawGizmos()
-        {
-            Gizmos.color = new Color(1, 0, 0, 0.5f);
-            Gizmos.DrawCube(transform.position + transform.up * 2, Vector3.one);
 
-            Gizmos.color = new Color(0, 1, 0, 1f);
-            Gizmos.DrawWireCube(transform.position + transform.right * 6, Vector3.one);
-
-            Gizmos.color = new Color(0, 0, 1, 0.75f);
-            Gizmos.DrawLine(transform.position + transform.up * 2, transform.position + transform.right * 6);
-        }
     }
 }
